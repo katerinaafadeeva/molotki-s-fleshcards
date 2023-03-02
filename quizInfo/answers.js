d0ad6cdbb5e07d51c829dffa3d3030de6e6cd467
Max Romanov <maxromanov@MacBook-Pro-Maksim.local>
2023-03-02 22:08:10 +0300

    Quiz questions complete

---
+++ quizInfo/answers.js
@@ -0,0 +1,40 @@
+const answersElbrus = [
+    {questionsId: 1, answers: 'понять рекурсию'},
+    {questionsId: 2, answers: 'носки'},
+    {questionsId: 3, answers: 'рыбалка'},
+    {questionsId: 4, answers: 'печенье'},
+    {questionsId: 5, answers: 'Айдима'},
+    {questionsId: 6, answers: 'Анатолий Павлович'},
+    {questionsId: 7, answers: 'кофемашина'},
+    {questionsId: 8, answers: 'эмоциональные качели'},
+    {questionsId: 9, answers: 'стул'},
+    {questionsId: 10, answers: 'деревянный тотем'},
+];
+
+const answersCats = [
+    {questionsId: 1, answers: 'Сэлем'},
+    {questionsId: 2, answers: 'Альф'},
+    {questionsId: 3, answers: 'правда'},
+    {questionsId: 4, answers: 'неправда'},
+    {questionsId: 5, answers: 'Аляска'},
+    {questionsId: 6, answers: '200 миллионов долларов'},
+    {questionsId: 7, answers: 'Бокс'},
+    {questionsId: 8, answers: 'сладкий'},
+    {questionsId: 9, answers: '500 миллионов'},
+    {questionsId: 10, answers: '57'},
+];
+
+const answersGeneral = [
+    {questionsId: 1, answers: 'Форрест Гамп'},
+    {questionsId: 2, answers: 'додо'},
+    {questionsId: 3, answers: 'Думская'},
+    {questionsId: 4, answers: 'Меркурий'},
+    {questionsId: 5, answers: 'Италия'},
+    {questionsId: 6, answers: 'Дырки'},
+    {questionsId: 7, answers: 'Ливерпуль'},
+    {questionsId: 8, answers: '0'},
+    {questionsId: 9, answers: 'фламинго'},
+    {questionsId: 10, answers: 'посадят в тюрьму'},
+];
+
+module.exports = answersElbrus, answersCats, answersGeneral;
---
+++ quizInfo/topics.js
@@ -0,0 +1,7 @@
+const topics = [
+    {topic: 'Жизнь Эльбруса'},
+    {topic: 'Все о котиках'},
+    {topic: 'Общие темы'}
+];
+
+module.exports = topics;
